d3ba0064dd6daf86e3cec9332697061ca39132e0
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-10-28 22:45:43 +0530

    untracked build files

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_RISC_V_Simulator_FILE D:/RISC_V_Simulator/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/RISC_V_Simulator.exe)
-set(__QT_DEPLOY_TARGET_RISC_V_Simulator_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_RISC_V_Simulator_RUNTIME_DLLS D:/QT/6.9.2/mingw_64/bin/Qt6Widgets.dll;D:/QT/6.9.2/mingw_64/bin/Qt6Gui.dll;D:/QT/6.9.2/mingw_64/bin/Qt6Core.dll)
