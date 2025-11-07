a40f47a86b82c81d3f7cdd1752d5c618c4237408
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-11-07 09:04:06 +0530

    added tests

--- test/rv5s_test.cpp
+++ test/rv5s_test.cpp
@@ -1,12 +1,44 @@
 #include <gtest/gtest.h>
 #include "../include/vm/rv5s_vms/rv5svm_nh_nf.h"
+#include "../include/vm/rv5s_vms/rv5svm_nh_f.h"
+#include "../include/vm/rv5s_vms/rv5svm_h_f.h"
+#include "../include/vm/rv5s_vms/rv5svm_h_nf.h"
 #include "../include/assembler/assembler.h"
 #include "../include/utils.h"
 
-TEST(FIVE_STAGE_VM_TEST,PipelineTest)
+TEST(FIVE_STAGE_VM_TEST,no_hazard_no_forwarding_test)
 {
     setupVmStateDirectory();
     RV5StageVM_NH_NF vm;
+    AssembledProgram program = assemble("../examples/pipeline_test_code.s");
+    vm.LoadProgram(program);
+    vm.DebugRun();    
+    
+}
+TEST(FIVE_STAGE_VM_TEST,no_hazard_forwarding_test)
+{
+    setupVmStateDirectory();
+    RV5StageVM_NH_F vm;
+    AssembledProgram program = assemble("../examples/pipeline_test_code.s");
+    vm.LoadProgram(program);
+    vm.DebugRun();    
+    
+}
+
+TEST(FIVE_STAGE_VM_TEST,harzard_no_forwarding_test)
+{
+    setupVmStateDirectory();
+    RV5StageVM_H_F vm;
+    AssembledProgram program = assemble("../examples/pipeline_test_code.s");
+    vm.LoadProgram(program);
+    vm.DebugRun();    
+    
+}
+
+TEST(FIVE_STAGE_VM_TEST,harzard_forwarding_test)
+{
+    setupVmStateDirectory();
+    RV5StageVM_H_NF vm;
     AssembledProgram program = assemble("../examples/pipeline_test_code.s");
     vm.LoadProgram(program);
     vm.DebugRun();    
