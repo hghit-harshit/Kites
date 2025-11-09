55f225468b084d14ca5d7706f903469e56bea20a
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-11-09 05:47:17 +0530

    added circuit view

---
+++ include/ui/circuit_view.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <QGraphicsView>
+#include <QDragEnterEvent>
+#include <QDragMoveEvent>
+#include <QDropEvent>
+
+namespace Kites
+{
+
+class CircuitView : public QGraphicsView
+{
+    Q_OBJECT
+
+public:
+    explicit CircuitView(QWidget* parent = nullptr);
+    explicit CircuitView(QGraphicsScene *scence, QWidget *parent = nullptr);
+protected:
+    //void dragEnterEvent(QDragEnterEvent* event) override;
+    //void dragMoveEvent(QDragMoveEvent *event)override;
+    //void dropEvent(QDropEvent *event)override;
+    //void mouseMoveEvent(QMouseEvent *mouseEvent) override;
+    void drawBackground(QPainter *painter, const QRectF &rect) override;
+};
+
+} // namespace Kites
--- src/ui/processor_designs/components/alu_item.cpp
+++
@@ -1,8 +0,0 @@
-#include "ui/processor_designs/components/alu_item.h"
-
-// namespace Kites
-// {
-//     ALUItem::ALUItem(QGraphicsItem* parent )
-    
-// }
-// delete this thing
