7a25b9a10064ef64781bcb2b1de5fd74867bb2c9
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-11-09 17:54:35 +0530

    feat: enhance ProcessorDialog to handle VMType selection and update UI

--- include/ui/processor_dialog.h
+++ include/ui/processor_dialog.h
@@ -2,7 +2,8 @@
 #define PROCESSOR_DIALOG_H
 
 #include <QDialog>
-
+#include "vm/vm_types.h"
+#include <QTreeWidgetItem>
 namespace Kites
 {
 namespace Ui {
@@ -14,11 +15,18 @@ class ProcessorDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit ProcessorDialog(QWidget *parent = nullptr);
+    explicit ProcessorDialog(QWidget *parent = nullptr,const VMType& currentVMType = VMType::RVSS);
     ~ProcessorDialog();
 
 private:
     Ui::ProcessorDialog *ui;
+    QTreeWidgetItem* m_currentSelectedItem = nullptr;
+private slots:
+    void on_buttonBox_accepted();
+    //void on_treeWidget_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
+
+signals:
+    void vmSelected(const VMType& vmType);
 };
 } // namespace Kites
 #endif // PROCESSOR_DIALOG_H
--- src/ui/processor_dialog.cpp
+++ src/ui/processor_dialog.cpp
@@ -3,16 +3,39 @@
 
 namespace Kites
 {
-ProcessorDialog::ProcessorDialog(QWidget *parent)
+ProcessorDialog::ProcessorDialog(QWidget *parent,const VMType& currentVMType)
     : QDialog(parent)
     , ui(new Ui::ProcessorDialog)
 {
     ui->setupUi(this);
     ui->treeWidget->setHeaderHidden(true);
+    ui->treeWidget->setCurrentItem(ui->treeWidget->topLevelItem(static_cast<int>(currentVMType)));
 }
 
 ProcessorDialog::~ProcessorDialog()
 {
     delete ui;
 }
+
+void ProcessorDialog::on_buttonBox_accepted()
+{
+    QTreeWidgetItem* selectedItem = ui->treeWidget->currentItem();
+    if(selectedItem)
+    {
+        m_currentSelectedItem = selectedItem; 
+        QString processorType = selectedItem->text(0);
+        if(processorType == "Single cycle processor")
+        {
+            emit vmSelected(VMType::RVSS);
+        }
+        else if(processorType == "5 statge Processor w/o hazard detection w/o forwarding")
+        {
+            emit vmSelected(VMType::RV5S);
+        }
+    }
+}
+
+
+
+
 } //namespace Kites
