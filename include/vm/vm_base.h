60c97bd283dc662a406d61a912d87564320c8f67
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-11-11 00:15:26 +0530

    refactored

--- include/vm/vm_base.h
+++ include/vm/vm_base.h
@@ -6,12 +6,11 @@
 #ifndef VM_BASE_H
 #define VM_BASE_H
 
-
 #include "memory_controller.h"
 #include "alu.h"
 #include "vm/registers.h"
 #include "vm_asm_mw.h"
-
+#include "ui/circuit_scene.h"
 #include <vector>
 #include <stack>
 #include <string>
@@ -22,114 +21,116 @@
 #include <queue>
 #include <atomic>
 #include <memory>
-enum SyscallCode {
-    SYSCALL_PRINT_INT = 1,
-    SYSCALL_PRINT_FLOAT = 2,
-    SYSCALL_PRINT_DOUBLE = 3,
-    SYSCALL_PRINT_STRING = 4,
-    SYSCALL_EXIT = 10,
-    SYSCALL_READ = 63,
-    SYSCALL_WRITE = 64,
+enum SyscallCode
+{
+	SYSCALL_PRINT_INT = 1,
+	SYSCALL_PRINT_FLOAT = 2,
+	SYSCALL_PRINT_DOUBLE = 3,
+	SYSCALL_PRINT_STRING = 4,
+	SYSCALL_EXIT = 10,
+	SYSCALL_READ = 63,
+	SYSCALL_WRITE = 64,
 };
 
 struct RegisterChange
 {
-  unsigned int reg_index;
-  unsigned int reg_type; // 0 for GPR, 1 for CSR, 2 for FPR
-  uint64_t old_value;
-  uint64_t new_value;
+	unsigned int reg_index;
+	unsigned int reg_type; // 0 for GPR, 1 for CSR, 2 for FPR
+	uint64_t old_value;
+	uint64_t new_value;
 };
 
 struct MemoryChange
 {
-  uint64_t address;
-  std::vector<uint8_t> old_bytes_vec;
-  std::vector<uint8_t> new_bytes_vec;
+	uint64_t address;
+	std::vector<uint8_t> old_bytes_vec;
+	std::vector<uint8_t> new_bytes_vec;
 };
 
 struct StepDelta
 {
-  uint64_t old_pc;
-  uint64_t new_pc;
-  std::vector<RegisterChange> register_changes;
-  std::vector<MemoryChange> memory_changes;
+	uint64_t old_pc;
+	uint64_t new_pc;
+	std::vector<RegisterChange> register_changes;
+	std::vector<MemoryChange> memory_changes;
 };
 
-/** 
+/**
  * @brief Base class for the virtual machine.
  */
-class VmBase {
+class VmBase
+{
 public:
-    VmBase() = default;
-    ~VmBase() = default;
+	VmBase() = default;
+	~VmBase() = default;
 
-    AssembledProgram program_;
-    std::atomic<bool> stop_requested_ = false;
-    std::mutex input_mutex_;
-    std::condition_variable input_cv_;
-    std::queue<std::string> input_queue_;
+	AssembledProgram program_;
+	std::atomic<bool> stop_requested_ = false;
+	std::mutex input_mutex_;
+	std::condition_variable input_cv_;
+	std::queue<std::string> input_queue_;
 
-    std::vector<uint64_t> breakpoints_;
+	std::vector<uint64_t> breakpoints_;
 
-    uint32_t current_instruction_{};
-    uint64_t program_counter_{};
-    
-    unsigned int cycle_s_{};
-    unsigned int instructions_retired_{};
-    float cpi_{};
-    float ipc_{};
-    unsigned int stall_cycles_{};
-    unsigned int branch_mispredictions_{};
+	uint32_t current_instruction_{};
+	uint64_t program_counter_{};
 
-    std::stack<StepDelta> undo_stack_;
-    std::stack<StepDelta> redo_stack_;
+	unsigned int cycle_s_{};
+	unsigned int instructions_retired_{};
+	float cpi_{};
+	float ipc_{};
+	unsigned int stall_cycles_{};
+	unsigned int branch_mispredictions_{};
 
-    StepDelta current_delta_;
-    std::string output_status_;
+	std::stack<StepDelta> undo_stack_;
+	std::stack<StepDelta> redo_stack_;
 
-    MemoryController memory_controller_;
-    RegisterFile registers_;
-    
-    alu::Alu alu_;
+	StepDelta current_delta_;
+	std::string output_status_;
 
+	MemoryController memory_controller_;
+	RegisterFile registers_;
 
-    void LoadProgram(const AssembledProgram &program);
-    uint64_t program_size_ = 0;
+	alu::Alu alu_;
 
-    uint64_t GetProgramCounter() const;
-    void UpdateProgramCounter(int64_t value);
-    
-    int32_t ImmGenerator(uint32_t instruction);
+	//todo make every file under kites namespace
+	Kites::CircuitScene circuit_scene_; // Circuit scene for visualization
+	void LoadProgram(const AssembledProgram &program);
+	uint64_t program_size_ = 0;
 
-    void AddBreakpoint(uint64_t val, bool is_line = true);
-    void RemoveBreakpoint(uint64_t val, bool is_line = true);
-    bool CheckBreakpoint(uint64_t address);
+	uint64_t GetProgramCounter() const;
+	void UpdateProgramCounter(int64_t value);
 
-    // void fetchInstruction();
-    // void decodeInstruction();
-    // void executeInstruction();
-    // void memoryAccess();
-    // void writeback();
+	int32_t ImmGenerator(uint32_t instruction);
 
-    // void HandleSyscall();
-    void PrintString(uint64_t address);
+	void AddBreakpoint(uint64_t val, bool is_line = true);
+	void RemoveBreakpoint(uint64_t val, bool is_line = true);
+	bool CheckBreakpoint(uint64_t address);
 
-    virtual void Run() = 0;
-    virtual void DebugRun() = 0;
-    virtual void Step() = 0;
-    virtual void Undo() = 0;
-    virtual void Redo() = 0;
-    virtual void Reset() = 0;
-    void DumpState(const std::filesystem::path &filename);
+	// void fetchInstruction();
+	// void decodeInstruction();
+	// void executeInstruction();
+	// void memoryAccess();
+	// void writeback();
 
-    void ModifyRegister(const std::string &reg_name, uint64_t value);
-    void PushInput(const std::string& input) 
-    {
-        std::lock_guard<std::mutex> lock(input_mutex_);
-        input_queue_.push(input);
-        input_cv_.notify_one();
-    }
+	// void HandleSyscall();
+	void PrintString(uint64_t address);
 
+	virtual void Run() = 0;
+	virtual void DebugRun() = 0;
+	virtual void Step() = 0;
+	virtual void Undo() = 0;
+	virtual void Redo() = 0;
+	virtual void Reset() = 0;
+	void DumpState(const std::filesystem::path &filename);
+
+	void ModifyRegister(const std::string &reg_name, uint64_t value);
+	void PushInput(const std::string &input)
+	{
+		std::lock_guard<std::mutex> lock(input_mutex_);
+		input_queue_.push(input);
+		input_cv_.notify_one();
+	}
 };
 
 #endif // VM_BASE_H
