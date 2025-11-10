7db168eac91d78c31ae3322a8e9b23444c00a101
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-11-11 01:00:36 +0530

    refac : removd extra code

---
+++ include/ui/processor_designs/rvss_circuit_scene.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "ui/circuit_scene.h"
+
+namespace Kites
+{
+class RVSSCircuitScene : public CircuitScene
+{
+    Q_OBJECT
+    public:
+        explicit RVSSCircuitScene(QObject *parent = nullptr);
+         
+    
+};
+} // namespace Kites
