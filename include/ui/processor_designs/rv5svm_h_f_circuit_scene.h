b438b5be18d751f3e7b5c524e5689cee285874a4
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2025-11-11 15:34:27 +0530

    fixed typo

--- include/ui/processor_designs/rv5svm_h_f_circuit_scene.h
+++ include/ui/processor_designs/rv5svm_h_f_circuit_scene.h
@@ -2,11 +2,11 @@
 #include "ui/circuit_scene.h"
 namespace Kites
 {
-class RV5StageVM_NH_F_CircuitScene : public CircuitScene
+class RV5StageVM_H_F_CircuitScene : public CircuitScene
 {
     Q_OBJECT
     public:
-        explicit RV5StageVM_NH_F_CircuitScene(QObject *parent = nullptr)
+        explicit RV5StageVM_H_F_CircuitScene(QObject *parent = nullptr)
         :CircuitScene(parent)
         {
             qDebug() << "Loading RVSS Circuit Scene";
