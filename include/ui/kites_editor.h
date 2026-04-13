d1cfbeb1a923a73f1eb24556e856345c57df4d29
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2026-04-14 04:39:14 +0530

    refac : added a base editor class for all editors in Kites

---
+++ include/ui/kites_editor.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <QPlainTextEdit>
+
+
+namespace Kites
+{
+/**
+ * @brief This will be the parent class for all code editors in Kites.
+ * It is basically providing a line number area to the editors and 
+ * 
+ */
+class KitesEditor : public QPlainTextEdit
+{
+    Q_OBJECT
+public:
+    explicit KitesEditor(QWidget *parent = nullptr);
+    ~KitesEditor(){};
+
+protected:
+class LineNumberArea : public QWidget
+{
+public:
+    LineNumberArea(KitesEditor *editor):m_editor(editor){};
+    QSize sizeHint() const override
+    {
+        return QSize(m_editor->lineNumberAreaWidth(), 0);
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override
+    {
+        m_editor->lineNumberAreaPaintEvent(event);
+    }
+
+private:
+    KitesEditor *m_editor;
+    
+};
+LineNumberArea *m_lineNumberArea = nullptr;
+void updateLineNumberAreaWidth(int newBlockCount);
+void updateLineNumberArea(const QRect &, int dy);
+int lineNumberAreaWidth();
+void lineNumberAreaPaintEvent(QPaintEvent *event);
+void highlightCurrentLine();
+void resizeEvent(QResizeEvent *event) override;
+};
+} // namespace Kites
