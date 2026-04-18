30d7de70bfb7f41612dcf77ecc7266d4da3fd326
hghit-harshit <144206069+hghit-harshit@users.noreply.github.com>
2026-04-19 02:12:25 +0530

    debug : added diffrent colors for stdout

---
+++ include/debug_colors.h
@@ -0,0 +1,12 @@
+/** @file debug_colors.h
+ *  @brief Debug color definitions for console output.
+ */
+#include <string>
+ 
+constexpr std::string RED = "\033[31m";
+constexpr std::string GREEN = "\033[32m";
+constexpr std::string YELLOW = "\033[33m";
+constexpr std::string BLUE = "\033[34m";
+constexpr std::string MAGENTA = "\033[35m";
+constexpr std::string CYAN = "\033[36m";
+constexpr std::string RESET = "\033[0m";
